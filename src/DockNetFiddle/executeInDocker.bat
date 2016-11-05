a361a6a2fe8a156c537049949be0201db6b0f41c
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2016-11-05 14:26:28 +0000

    Run program sending command to pre-started coderunner container

--- src/DockNetFiddle/executeInDocker.bat
+++ src/DockNetFiddle/executeInDocker.bat
@@ -3,12 +3,7 @@
 REM setup docker environment so docker is available in this cmd session
 FOR /f "tokens=*" %%i IN ('docker-machine env default') DO @%%i
 
-REM run program using docker dotnet:latest image and automatically remove image
-REM docker options
-REM		--rm						remove container once finishes executing the default command
-REM		-t							attach terminal so we get standard output back into cmd
-REM		-v %programfolder%:/app		mounts folder passed as argument as the /app folder inside the container
-REM		-w /app						sets working directory inside container
-REM		microsoft/dotnet:latest		creates container from latest microsoft/dotnet image (official .net core image)
-REM		/bin/sh	-c "..."			container entry point program (bash) with extra arguments (command to be executed in the bash)
-docker run --rm -t -v %1:/app -w /app microsoft/dotnet /bin/sh -c "dotnet restore && dotnet run"
+REM Execute command in running container named "coderunner"
+SET folderName=%1
+SET "dockerCommand=cd /requests/%folderName% && dotnet restore && dotnet run"
+docker exec coderunner /bin/sh -c "%dockerCommand%"
