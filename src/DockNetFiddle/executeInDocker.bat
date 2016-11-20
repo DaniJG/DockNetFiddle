41e48599a92f9242e352a6e997d570c2e07bef76
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2016-11-20 11:50:06 +0000

    rename compose as default file name docker-compose.yml

--- src/DockNetFiddle/executeInDocker.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-
-REM setup docker environment so docker is available in this cmd session
-FOR /f "tokens=*" %%i IN ('docker-machine env default') DO @%%i
-
-REM Execute command in running container named "coderunner"
-SET zipFileName=/requests/%1
-SET "dockerCommand=./coderunner.sh %zipFileName%"
-docker exec coderunner /bin/sh -c "%dockerCommand%"
